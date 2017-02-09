4647ded72a8e50c307d1e3ae69bcc5f9bcb7871e
cuongtranba <bacuongtr@gmail.com>
2017-02-09 21:51:24 +0700

    add genres router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var genres = require('./routes/genres');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', index);
 app.use('/users', users);
+app.use('/genres',genres)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
